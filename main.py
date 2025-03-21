7866c7dc91a85f3e766fe8b5a81069a9dbdd36c9
Plazmer <praw2003@gmail.com>
2025-03-21 21:12:41 +0000

    устранение ошибок pylint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import ast
-
 """Модуль main.py: пример использования Pylint."""
+
+import ast
 
 user_input = input("Введите число: ")
 number = ast.literal_eval(user_input)  # Безопасное преобразование
