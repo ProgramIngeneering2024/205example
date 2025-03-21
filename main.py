89c61853daaae17830e92a16cb759c1fac07cda8
Plazmer <praw2003@gmail.com>
2025-03-21 21:08:10 +0000

    Устранение базовых ошибок

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""Модуль main.py: пример использования Pylint."""
+
 # Пример уязвимости для Bandit (eval())
 user_input = input("Введите выражение: ")
+
+# pylint: disable=eval-used
 eval(user_input)  # Bandit обнаружит это как уязвимость
