63a6d7afa4de0835bae1b548b1d3147148164ca2
Plazmer <praw2003@gmail.com>
2025-03-21 21:10:30 +0000

    Устранение ошибок bandit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import ast
+
 """Модуль main.py: пример использования Pylint."""
 
-# Пример уязвимости для Bandit (eval())
-user_input = input("Введите выражение: ")
-
-# pylint: disable=eval-used
-eval(user_input)  # Bandit обнаружит это как уязвимость
+user_input = input("Введите число: ")
+number = ast.literal_eval(user_input)  # Безопасное преобразование
+print(number * 2)
